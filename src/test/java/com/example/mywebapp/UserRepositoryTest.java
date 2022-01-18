7086566a9b5b56a763db26fdca73d78caa2e3d91
Your Name <you@example.com>
2022-01-18 14:40:39 +0200

    update project

---
+++ src/test/java/com/example/mywebapp/UserRepositoryTest.java
@@ -0,0 +1,41 @@
+package com.example.mywebapp;
+
+import com.example.mywebapp.model.User;
+import com.example.mywebapp.repository.UserRepository;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.annotation.Rollback;
+
+import javax.persistence.Id;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Rollback(false)
+public class UserRepositoryTest {
+    @Autowired
+    private UserRepository repository;
+
+    @Test
+    public void testAddNew() {
+        User user = new User();
+        user.setEmail("all@gmail.com");
+        user.setPassword("all23214");
+        user.setFirstName("All");
+        user.setLastName("None");
+
+        User savedUser = repository.save(user);
+
+        Assertions.assertThat(savedUser).isNotNull();
+        Assertions.assertThat(savedUser.getId()).isGreaterThan(0);
+    }
+
+    @Test
+    public void testListAll(){
+
+    }
+
+
+}
