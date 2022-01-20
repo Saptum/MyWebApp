abe443dc69ffdfc6ebc96c7a7ad115134a1afb83
Your Name <you@example.com>
2022-01-20 13:14:03 +0200

    update project

--- src/main/java/com/example/mywebapp/controller/UserController.java
+++ src/main/java/com/example/mywebapp/controller/UserController.java
@@ -1,11 +1,13 @@
 package com.example.mywebapp.controller;
 
 import com.example.mywebapp.model.User;
+import com.example.mywebapp.service.UserNotFoundException;
 import com.example.mywebapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -27,12 +29,30 @@ public class UserController {
     @GetMapping("/users/new")
     public String showNewForm(Model model) {
         model.addAttribute("user", new User());
+        model.addAttribute("pageTitle","Add New User");
         return "user_form";
     }
 
     @PostMapping("/users/save")
-    public String saveUser(User user) {
+    public String saveUser(User user,RedirectAttributes redirectAttributes) {
         service.save(user);
+        redirectAttributes.addFlashAttribute("message","The user has been saved successfully.");
         return "redirect:/users";
     }
+
+    @GetMapping("/users/edit/{id}")
+    public String showEditForm(@PathVariable("id") Integer id, Model model, RedirectAttributes redirectAttributes) {
+        try {
+            User user = service.get(id);
+            model.addAttribute("user",user);
+            model.addAttribute("pageTitle","Edit User (ID: " + id +")");
+
+            return "user_form";
+        }catch (UserNotFoundException exception){
+            redirectAttributes.addFlashAttribute("message","The user has been saved successfully.");
+            return "redirect:/users";
+        }
+    }
+
+
 }
--- src/main/java/com/example/mywebapp/service/UserService.java
+++ src/main/java/com/example/mywebapp/service/UserService.java
@@ -6,17 +6,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     @Autowired
     private UserRepository repository;
 
-    public List<User> listAll(){
+    public List<User> listAll() {
         return (List<User>) repository.findAll();
     }
 
-    public void save(User user){
+    public void save(User user) {
         repository.save(user);
     }
+
+    public User get(Integer id) throws UserNotFoundException {
+        Optional<User> result = repository.findById(id);
+        if (result.isPresent()) {
+            return result.get();
+        }
+        throw new UserNotFoundException("Could not find any users with ID " + id);
+    }
+
+
 }
